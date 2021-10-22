8f2df2948961b76cabaf7f0ff76028a94ee7d273
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-10-22 17:39:10 +0200

    feat: :construction_worker: Created match class

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {Match} from './match';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Constructor', () => {
+  let match = new Match("Salcedo", "Chema");
+  expect(match.getNamePlayer1()).toBe("Salcedo");
 });
+
+test('PlayerScore1', () => {
+  let score = 123;
+  let match = new Match("Salcedo", "Chema");
+  match.setScorePlayer1(score);
+  expect(match.getScorePlayer1()).toBe(123);
+});
+
+test('PlayerScore2', () => { 
+  let score = 11;
+  let match = new Match("Salcedo","Chema");
+  match.setScorePlayer2(score);
+  expect(match.getScorePlayer2()).toBe(11);
+})
